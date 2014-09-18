5b37eabe1d43a187de5f72fe277b85e0ca22bf81
piper4123w <kyle.lawson@marquette.edu>
2014-09-17 21:21:27 -0500

    pushing

--- COEN 2010/src/Homework2/HelpDesk.java
+++ COEN 2010/src/Homework2/HelpDesk.java
@@ -1,40 +1,107 @@
 package Homework2;
 
-public class HelpDesk {
+// import java.util.Stack;
 
-	
-	public void step(){
-		/*TODO:
-		 * Advance the simulation one minute
-		 */
-	}
-	
-	public void addStudent(String name, int course, int workload){
-			/*TODO:
-			 * Add an arriving student with the indicate name, course number, and minutes of workload. If the 
-current student has a lower or equal course number, the arriving student is turned away. If the 
-current student has a higher course number, the current student is preempted while the new 
-student is helped.
-			 */
-		
-	}
-	public int getTime(){
-		/*TODO:
-		 * Get the current simulation time in minutes.
-		 */
-		
-	}
-	public String toString(){
-		/*TODO:
-		 * Return the status of the simulation. This produces strings like, "Time 2, Helping Jack 
-from COSC1010", or "Time 9, IDLE".
-		 */
-		
-	}
-	public String getLog(){
-		/*TODO:
-		 * Return the entire HelpDesk session log
-		 */
-		
-	}
+/**
+ * @author Kyle
+ * @param <E>
+ *
+ */
+public class HelpDesk {
+  private int    time       = 0;
+  private int    course     = 0;
+  private int    workload   = 0;
+  private String name       = "";
+  private int    index      = 0;
+
+  Student        logStudent = new Student(name, course, workload, time);
+
+  public void step() {
+    time++;
+  }
+
+  /**
+   * @param name
+   * @param course
+   * @param workload
+   */
+  public void addStudent(String name, int course, int workload) {
+    Student student = new Student(name, course, workload, time);
+  }
+
+  /**
+   * @return
+   */
+  public int getTime() {
+    return time;
+  }
+
+  public String toString() {
+    String sendBack = "Time: " + this.time + ",";
+    if (this.name == "") {
+      String sendBackAgain = sendBack + "IDLE";
+      return sendBackAgain;
+    }
+    else {
+      String sendBackAgain = sendBack + "Helping " + this.name + "from " + this.course;
+      return sendBackAgain;
+    }
+    // Return the status of the simulation. This produces strings like, "Time 2, Helping Jack
+    // from COSC1010", or "Time 9, IDLE".
+  }
+
+  public String getLog() {
+
+  }
+
+  public int getCourse() {
+    return time;
+  }
+}
+
+class Student {
+  private String name;
+  private int    course;
+  private int    workload;
+  private int    arriveTime;
+
+  Student(String name, int course, int workload, int time) {
+    // TODO Auto-generated constructor stub
+    this.name = name;
+    this.course = course;
+    this.workload = workload;
+    this.arriveTime = time;
+  }
+
+  public int getTime() {
+    return arriveTime;
+  }
+
+  public int getCourse() {
+    return course;
+  }
+
+  public int getWork() {
+    return workload;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setTime(int time) {
+    this.arriveTime = time;
+  }
+
+  public void setCourse(int course) {
+    this.course = course;
+  }
+
+  public void setWork(int work) {
+    this.workload = work;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
