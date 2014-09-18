1d518e8374369c382dd62fd08c7e864c08aa9754
jReality <jisawesome1@yahoo.com>
2014-09-18 02:32:37 -0500

    changes to the final project

--- COEN 2010/src/Homework2/HelpDesk.java
+++ COEN 2010/src/Homework2/HelpDesk.java
@@ -1,96 +1,268 @@
 package Homework2;
 
-
-
 public class HelpDesk {
-  private int    time       = 0;
-  private int    course     = 0;
-  private int    workload   = 0;
-  private String name       = "";
-  private int    index      = 0;
+	private String            name         = null;
+	private int               time         = 0;
+	private int               work         = 0;
+	private int               course       = 0;
 
-  Student logStudent = new Student(name, course, workload, time);
+	DSLinkedStack<Student>    StudentStack = new DSLinkedStack<Student>();
+	DSLinkedStack<LogStudent> LogStack     = new DSLinkedStack<LogStudent>();
 
-  public void step() {
-    time++;
-  }
+	public String toString() {
+		String returnString = "Time " + time + ",";
+		if (StudentStack.isEmpty()) {
+			returnString = returnString + "IDLE";
+		}
+		else {
+			Student student = null;
+			try {
+				student = StudentStack.top();
+			}
+			catch (StackUnderflowException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			returnString = returnString + "Helping " + student.getName() + " from COSC" + student.getCourse();
+		}
+		return returnString;
+	}
 
- 
-  public void addStudent(String name, int course, int workload) {
-    Student student = new Student(name, course, workload, time);
-  }
+	public void step() {
+		if (!StudentStack.isEmpty()) {
+			try {
+				Student student = StudentStack.top();
+				work = student.getWork() - 1;
+				student.setWork(work);
+				if (work == 0) {
+					String logName = student.getName();
+					int logCourse = student.getCourse();
+					int logTime = time;
+					LogStudent logStudent = new LogStudent(logName, logCourse, logTime, false);
+					LogStack.push(logStudent);
+					StudentStack.pop();
+				}
+			}
+			catch (StackUnderflowException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		time++;
+	}
 
- 
-  public int getTime() {
-    return time;
-  }
+	public String getLog() {
+		String logString = "";
+		LogStudent logStudent = null;
+		while (!LogStack.isEmpty()) {
+			logString = "";
+			try {
+				logStudent = LogStack.top();
+			}
+			catch (StackUnderflowException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			logString = "Time: " + logStudent.getLogTime() + ", ";
+			if (logStudent.getArriving()) {
+				logString = logString + "Started";
+			}
+			else {
+				logString = logString + "Finished";
+			}
+			logString = logString + " helping " + logStudent.getLogName() + " from COSC" + logStudent.getLogCourse();
+			System.out.println(logString);
+			try {
+				LogStack.pop();
+			}
+			catch (StackUnderflowException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return "";
+	}
 
-  public String toString() {
-    String sendBack = "Time: " + this.time + ",";
-    if (this.name == "") {
-      String sendBackAgain = sendBack + "IDLE";
-      return sendBackAgain;
-    }
-    else {
-      String sendBackAgain = sendBack + "Helping " + this.name + "from " + this.course;
-      return sendBackAgain;
-    }
-    // Return the status of the simulation. This produces strings like, "Time 2, Helping Jack
-    // from COSC1010", or "Time 9, IDLE".
-  }
+	public void addStudent(String name, int course, int work) {
+		Student student = new Student(name, course, work);
+		if (!StudentStack.isEmpty()) {
+			try {
+				Student oldStudent = StudentStack.top();
+				if (student.getCourse() < oldStudent.getCourse()) {
+					StudentStack.push(student);
+					LogStudent logStudent = new LogStudent(name, course, time, true);
+					LogStack.push(logStudent);
+				}
+			}
+			catch (StackUnderflowException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else {
+			StudentStack.push(student);
+			LogStudent logStudent = new LogStudent(name, course, time, true);
+			LogStack.push(logStudent);
+		}
 
-  public String getLog() {
+	}
 
-  }
+	public int getTime() {
+		return time;
+	}
+}
 
-  public int getCourse() {
-    return time;
-  }
+class LogStudent {
+	private int     Ltime;
+	private int     Lcourse;
+	private String  Lname;
+	private boolean isArriving;
+
+	LogStudent(String name, int course, int time, boolean isArriving) {
+		this.Lname = name;
+		this.Lcourse = course;
+		this.Ltime = time;
+		this.isArriving = isArriving;
+	}
+
+	public int getLogTime() {
+		return Ltime;
+	}
+
+	public int getLogCourse() {
+		return Lcourse;
+	}
+
+	public String getLogName() {
+		return Lname;
+	}
+
+	public boolean getArriving() {
+		return isArriving;
+	}
+
+	public void setArriving(boolean status) {
+		isArriving = status;
+	}
 }
 
 class Student {
-  private String name;
-  private int    course;
-  private int    workload;
-  private int    arriveTime;
+	private int    Swork;
+	private int    Scourse;
+	private String Sname;
 
-  Student(String name, int course, int workload, int time) {
-    // TODO Auto-generated constructor stub
-    this.name = name;
-    this.course = course;
-    this.workload = workload;
-    this.arriveTime = time;
-  }
+	Student(String name, int course, int work) {
+		this.Sname = name;
+		this.Scourse = course;
+		this.Swork = work;
+	}
 
-  public int getTime() {
-    return arriveTime;
-  }
+	public String getName() {
+		return Sname;
+	}
 
-  public int getCourse() {
-    return course;
-  }
+	public int getCourse() {
+		return Scourse;
+	}
 
-  public int getWork() {
-    return workload;
-  }
+	public int getWork() {
+		return Swork;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public void setWork(int newWork) {
+		Swork = newWork;
+	}
+}
 
-  public void setTime(int time) {
-    this.arriveTime = time;
-  }
+// ------------------------------------------------------------------//
+class DSLinkedStack<T> implements DSUnboundedStackInterface<T> {
+	private DSLLNode<T> top;
 
-  public void setCourse(int course) {
-    this.course = course;
-  }
+	public DSLinkedStack() {
+		top = null;
+	}
 
-  public void setWork(int work) {
-    this.workload = work;
-  }
+	public void push(T element) {
+		top = new DSLLNode<T>(element, top);
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void pop() throws StackUnderflowException {
+		if (!isEmpty())
+			top = top.getNext();
+		else
+			throw new StackUnderflowException("Top attempted on empty stack.");
+	}
+
+	public T top() throws StackUnderflowException {
+		if (!isEmpty())
+			return top.getData();
+		else
+			throw new StackUnderflowException("Top attempted on empty stack.");
+	}
+
+	public boolean isEmpty() {
+		return (top == null);
+	}
+}
+
+interface DSUnboundedStackInterface<T> extends DSStackInterface<T> {
+	void push(T element);
+}
+
+interface DSStackInterface<T> {
+	void pop() throws StackUnderflowException;
+
+	T top() throws StackUnderflowException;
+
+	boolean isEmpty();
+}
+
+class DSLLNode<T> {
+	private T           data;
+	private DSLLNode<T> next;
+
+	public DSLLNode(T data) {
+		this(data, null);
+	}
+
+	public DSLLNode(T data, DSLLNode<T> next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public void setNext(DSLLNode<T> next) {
+		this.next = next;
+	}
+
+	public T getData() {
+		return this.data;
+	}
+
+	public DSLLNode<T> getNext() {
+		return this.next;
+	}
+}
+
+interface DSBoundedStackInterface<T> extends DSStackInterface<T> {
+	void push(T element) throws StackOverflowException;
+
+	boolean isFull();
+}
+
+class StackOverflowException extends Exception {
+	public StackOverflowException(String s) {
+		super(s);
+	}
+
+}
+
+class StackUnderflowException extends Exception {
+
+	public StackUnderflowException(String s) {
+		super(s);
+	}
 }
